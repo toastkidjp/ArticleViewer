8b87d409004f8b23d71e07ae8b7ae262dd8ee932
Toast Kid <super_q1m@yahoo.co.jp>
2019-05-01 22:35:40 +0900

    refactor Clean up ZipSearcher.

--- app/src/main/java/jp/toastkid/diary/search/ZipSearcher.kt
+++ app/src/main/java/jp/toastkid/diary/search/ZipSearcher.kt
@@ -8,7 +8,6 @@ import java.io.InputStream
 import java.nio.charset.Charset
 import java.util.zip.ZipInputStream
 
-
 /**
  * @author toastkidjp
  */
@@ -20,7 +19,6 @@ object ZipSearcher {
         ZipInputStream(inputStream, Charset.forName("UTF-8"))
             .also { zipInputStream ->
                 var nextEntry = zipInputStream.nextEntry
-                Timber.i("next file: ${nextEntry.name}")
                 while (nextEntry != null) {
                     Okio.buffer(Okio.source(zipInputStream)).also {
                         val content = it.readUtf8()
@@ -29,7 +27,6 @@ object ZipSearcher {
                             val title =
                                 NameDecoder(name.substring(name.indexOf("/") + 1, name.lastIndexOf(".")))
                             results.add(DictionaryFile(title, content))
-                            //Timber.i(nextEntry.name + ": " + if (content.length < 100) { content } else {content.substring(0, 100) })
                         }
                     }
                     nextEntry = try {
@@ -43,29 +40,6 @@ object ZipSearcher {
                 zipInputStream.closeEntry()
             }
         return results
-/*
-try (ZipInputStream zipIn = new ZipInputStream(inputStream,Charset.forName("SJIS"))) {
-
-        // zip内のファイルがなくなるまで読み続ける
-        while (null != zipIn.getNextEntry()) {
-
-            String line;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(zipIn, "Shift_JIS"));
-            while(null != (line = reader.readLine())){
-
-                String[] data = line.split(":");
-
-                String fruit = data[0];
-                int value = Integer.parseInt(data[1]);
-
-                // すでに果物があれば加える。なければ、そのまま代入する。
-                result.computeIfPresent(fruit, (k,v) -> v + value);
-                result.putIfAbsent(fruit, value);
-            }
-            zipIn.closeEntry();
-        }
-    }
- */
     }
 
 }
