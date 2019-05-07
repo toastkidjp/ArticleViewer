42ff4da48f6c159a5e18c1773cc4065746a182cd
Toast Kid <super_q1m@yahoo.co.jp>
2019-05-07 21:03:25 +0900

    refactor #3 Delete unused comment from ArticleListFragment.

--- app/src/main/java/jp/toastkid/article_viewer/article/list/ArticleListFragment.kt
+++ app/src/main/java/jp/toastkid/article_viewer/article/list/ArticleListFragment.kt
@@ -185,9 +185,6 @@ class ArticleListFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_all_article -> {
                 all()
