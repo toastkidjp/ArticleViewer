8a533f923547807ac7eb3051032af05798c79c6f
Toast Kid <super_q1m@yahoo.co.jp>
2019-06-22 21:00:09 +0900

    refactor #22 Add TitleFilterGeneratorTest.

--- app/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
@@ -1,8 +1,18 @@
 package jp.toastkid.article_viewer.calendar
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * @author toastkidjp
  */
-class TitleFilterGeneratorTest
+class TitleFilterGeneratorTest {
+
+    @Test
+    fun test() {
+        assertEquals("日記2019-01-01%", TitleFilterGenerator(2019, 1, 1))
+        assertEquals("日記2019-10-10%",TitleFilterGenerator(2019, 10, 10))
+        assertEquals("日記2019-09-09%",TitleFilterGenerator(2019, 9, 9))
+        assertEquals("日記2019-11-11%",TitleFilterGenerator(2019, 11, 11))
+    }
+}
