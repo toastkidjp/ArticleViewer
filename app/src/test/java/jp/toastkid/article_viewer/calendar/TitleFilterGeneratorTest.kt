725aad62d602eaf3358050976633969512570d7f
Toast Kid <super_q1m@yahoo.co.jp>
2019-06-30 23:39:29 +0900

    refactor #23 Clean up test code.

--- app/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
+++ app/src/test/java/jp/toastkid/article_viewer/calendar/TitleFilterGeneratorTest.kt
@@ -10,9 +10,21 @@ class TitleFilterGeneratorTest {
 
     @Test
     fun test() {
-        assertEquals("日記2019-01-01%", TitleFilterGenerator(2019, 1, 1))
-        assertEquals("日記2019-10-10%",TitleFilterGenerator(2019, 10, 10))
-        assertEquals("日記2019-09-09%",TitleFilterGenerator(2019, 9, 9))
-        assertEquals("日記2019-11-11%",TitleFilterGenerator(2019, 11, 11))
+        assertEquals(
+            "日記2019-01-01%",
+            TitleFilterGenerator(2019, 1, 1)
+        )
+        assertEquals(
+            "日記2019-10-10%",
+            TitleFilterGenerator(2019, 10, 10)
+        )
+        assertEquals(
+            "日記2019-09-09%",
+            TitleFilterGenerator(2019, 9, 9)
+        )
+        assertEquals(
+            "日記2019-11-11%",
+            TitleFilterGenerator(2019, 11, 11)
+        )
     }
 }
