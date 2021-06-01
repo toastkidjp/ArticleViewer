f93c1f4bbc305872ee24fb1c04d52d44004e1cbe
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-02 07:12:17 +0900

    refactor #31 Fix imports.

--- app/src/main/java/jp/toastkid/article_viewer/article/list/ArticleListFragment.kt
+++ app/src/main/java/jp/toastkid/article_viewer/article/list/ArticleListFragment.kt
@@ -11,7 +11,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
