9de73e89cc239911bc844d6a2de79bf28c04553c
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-02 07:04:50 +0900

    feature #31 Adapt new format.

--- app/src/main/java/jp/toastkid/article_viewer/calendar/TitleFilterGenerator.kt
+++ app/src/main/java/jp/toastkid/article_viewer/calendar/TitleFilterGenerator.kt
@@ -17,6 +17,6 @@ object TitleFilterGenerator {
     operator fun invoke(year: Int, month: Int, date: Int): String {
         val monthStr = if (month < 10) "0$month" else month.toString()
         val dateStr = if (date < 10) "0$date" else date.toString()
-        return "日記$year-$monthStr-$dateStr%"
+        return "$year-$monthStr-$dateStr%"
     }
 }
