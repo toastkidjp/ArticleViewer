4ea616a0c5361f7f42edc9cc3353e9c28f61f819
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-04 07:17:52 +0900

    refactor #31 Use explicit imports.

--- app/src/test/java/jp/toastkid/article_viewer/ExampleUnitTest.kt
+++ app/src/test/java/jp/toastkid/article_viewer/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package jp.toastkid.article_viewer
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
