3d1fbade6789ff8c56d8c712414d89fb564688db
Toast Kid <super_q1m@yahoo.co.jp>
2021-06-09 07:20:29 +0900

    refactor #31 Add function comment.

--- app/src/main/java/jp/toastkid/article_viewer/calendar/TitleFilterGenerator.kt
+++ app/src/main/java/jp/toastkid/article_viewer/calendar/TitleFilterGenerator.kt
@@ -22,6 +22,11 @@ class TitleFilterGenerator {
         return "$year-$monthStr-$dateStr%"
     }
 
+    /**
+     * If you passed under 10 digit, this function will return with added 0.
+     * @param digit any digit
+     * @return string
+     */
     private fun formatDigit(digit: Int) = if (digit < 10) "0$digit" else digit.toString()
 
 }
